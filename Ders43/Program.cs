2581d932638baaad368de177a0f024491d4338fd
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-10 02:45:04 +0300

    Ders43

---
+++ Ders43/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ders43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tahmin = 0;
+            int hafiza;
+            int sayac = 0;
+            Random rastgele = new Random();
+            hafiza = rastgele.Next(1, 100);
+            while (tahmin != hafiza)
+            {
+                sayac++;
+                Console.Write("Sayıyı Girin: ");
+                tahmin = Convert.ToInt16(Console.ReadLine());
+                if (tahmin > hafiza)
+                {
+                    Console.WriteLine("Daha küçük sayı girin...");
+                }
+                else if (tahmin < hafiza)
+                {
+                    Console.WriteLine("Daha büyük sayı girin...");
+                }
+            }
+            Console.WriteLine("Tebriklerrrrr...");
+            Console.WriteLine("{0}. denemede buldunuz", sayac);
+            Console.Read();
+        }
+    }
+}
