5eeb02059b5a3793e623eef221168bdbcc0d2022
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-10 03:06:00 +0300

    Ders44

---
+++ Ders44/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Media;
+using System.Diagnostics;
+namespace Ders44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer ses = new SoundPlayer();
+            string yol;
+            yol = @"C:\Users\semav\OneDrive\Desktop\Piyano-Dinletisi-_Dalga-Sesi.wav";
+            ses.SoundLocation = yol;
+            ses.Play();
+            Console.Read();
+            //Process.Start("C:\\Program Files (x86)\\Windows Media Player\\wmplayer.exe", @"C:\Users\semav\OneDrive\Desktop\Piyano-Dinletisi-_Dalga-Sesi.wav");
+        }
+    }
+}
