a8dff5cf316bdf010a84bb0184b43abcc21157ec
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-11 20:46:34 +0300

    Ders49

---
+++ Ders49/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ders49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = 11;
+            int i = Convert.ToInt32(o);
+            Console.Write(o);
+        }
+    }
+}
