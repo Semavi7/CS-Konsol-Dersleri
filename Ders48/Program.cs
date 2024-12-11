82d11f1b435a7753047e309325dddaf242174829
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-11 20:43:59 +0300

    Ders48

---
+++ Ders48/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+namespace Ders48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gun;
+            Console.Write("Gün Nosu Girin: ");
+            gun = Convert.ToInt16(Console.ReadLine());
+            StreamWriter dosya = new StreamWriter($@"D:\Günlük\Gün{gun}.txt");
+            string veri;
+            Console.WriteLine("Buraya Notu Yazın: ");
+            veri = Console.ReadLine();
+            dosya.WriteLine(veri);
+            dosya.Close();
+        }
+    }
+}
