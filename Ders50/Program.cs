1ca63aa1943281b246ca13980698342bc731e282
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-11 20:48:08 +0300

    Ders50

---
+++ Ders50/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ders50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tebrikler C# Konsol Eğitimi Bitti");
+        }
+    }
+}
